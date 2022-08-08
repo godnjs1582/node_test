1c7138758abe92a7436d012e116394aaff274f70
kwonhaewon <kwonhaewon@kwonhaewons-MacBook-Pro.local>
2022-08-08 14:35:16 +0900

    node.js basic

---
+++ server.js
@@ -0,0 +1,23 @@
+// console.log("hello world")
+// console.log(global);
+
+const os =require('os');
+const path = require('path');
+const { add, substract, multiply,divide } = require('./math');
+
+console.log(add(3,4))
+console.log(substract(3,4))
+console.log(multiply(3,4))
+console.log(divide(20,4))
+// console.log(os.type());
+// console.log(os.version());
+// console.log(os.homedir());
+
+// console.log(__dirname);
+// console.log(__filename);
+
+// console.log(path.dirname(__filename))
+// console.log(path.basename(__filename))
+// console.log(path.extname(__filename))
+
+// console.log(path.parse(__filename))
